e095eb3d0410f7a43ae23fc79595ba33615f3539
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2022-01-28 16:20:50 +0600

    First commit

---
+++ intro.js
@@ -0,0 +1,47 @@
+var onionPrice = "30.56";
+var potatoPrice = "45.56";
+var totalPrice = parseInt(onionPrice) + parseInt(potatoPrice);
+console.log(totalPrice);
+
+var bookPrice = "101.25";
+var penPrice = "80.85";
+
+var totalPrice = parseFloat(bookPrice) + parseFloat(penPrice);
+var totalPriceWithOutFloat = parseInt(totalPrice);
+console.log(totalPriceWithOutFloat);
+
+var tShirtPrice = 200;
+
+console.log(typeof tShirtPrice);
+
+var fullName = "Asrafuzzaman Suvon";
+console.log(typeof fullName);
+
+var isSheComing = true;
+console.log(typeof isSheComing);
+
+var day = new Date();
+console.log(typeof day);
+
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total = total.toFixed(1);
+total = parseFloat(total);
+console.log(total);
+
+// program to print a sentence
+var firstName = "Asrafuzzaman";
+var lastName = "Suvon";
+var fullName = firstName + " " + lastName;
+console.log("Hi " + fullName + " Welcome to JS World");
+
+var salary = 15;
+var man = 7;
+var remainning = salary % man;
+console.log(remainning);
+
+
+
+
+
