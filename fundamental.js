96c5dd4f8e921c1c5ad478e1170a0d9c601600b7
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2022-01-30 01:36:42 +0600

    Added fundamental js

--- fundamental.js
+++ fundamental.js
@@ -1,24 +1,24 @@
 var cousinNames = ["Shantonu", "Adi", "Tousif", "Sourav", "Mithoon", "Sumon", "Shila", "Rubab"];
-// console.log(cousinNames);
-// console.log(cousinNames.length);
+console.log(cousinNames);
+console.log(cousinNames.length);
 
-// var findIndex = cousinNames.indexOf("Rubab");
-// console.log(findIndex);
+var findIndex = cousinNames.indexOf("Rubab");
+console.log(findIndex);
 
-// var souravIndex = cousinNames.indexOf("Sourav");
-// console.log(souravIndex);
+var souravIndex = cousinNames.indexOf("Sourav");
+console.log(souravIndex);
 
-// var antrixKaDakuIndex = cousinNames.indexOf("Antrix Ka Daku");
-// console.log(antrixKaDakuIndex);
+var antrixKaDakuIndex = cousinNames.indexOf("Antrix Ka Daku");
+console.log(antrixKaDakuIndex);
 
 var fifthPosition = cousinNames[5];
-// console.log(fifthPosition);
+console.log(fifthPosition);
 
 cousinNames[0] = "Shantonu Hawlader";
 
 cousinNames[4] = "Mithoon Loskor"
 
-// console.log(cousinNames);
+console.log(cousinNames);
 
 
 var friendsAge = [23, 25, 30, 33, 24, 29, 28];
@@ -28,7 +28,7 @@ friendsAge.pop();
 
 var lastItem = friendsAge.pop();
 
-// console.log(lastItem);
+console.log(lastItem);
 
 friendsAge.unshift(45);
 friendsAge.unshift(50);
@@ -36,16 +36,16 @@ friendsAge.unshift(50);
 var firstItem = friendsAge.shift();
 var secondItem = friendsAge.shift();
 
-// console.log(firstItem, secondItem, friendsAge);
+console.log(firstItem, secondItem, friendsAge);
 
 var ageSerialled = friendsAge.sort();
 
-// console.log(friendsAge);
-// console.log(ageSerialled);
+console.log(friendsAge);
+console.log(ageSerialled);
 
 var reverseAge = ageSerialled.reverse();
 
-// console.log(reverseAge);
+console.log(reverseAge);
 
 
 
@@ -57,59 +57,59 @@ var reverseAge = ageSerialled.reverse();
 var friendAge = 25;
 var myAge = 26;
 
-// console.log(friendAge > myAge);
-// console.log(friendAge < myAge);
-// console.log(friendAge == myAge);
-// console.log(friendAge != myAge);
-// console.log(Goodjob && bigHome);
-// console.log(GoodMan || Badman);
+console.log(friendAge > myAge);
+console.log(friendAge < myAge);
+console.log(friendAge == myAge);
+console.log(friendAge != myAge);
+console.log(Goodjob && bigHome);
+console.log(GoodMan || Badman);
 
 var chickenFryPrice = 600;
 var potatoFry = 100;
 var myBudget = 560;
 
-// if (chickenFryPrice > myBudget)
-// {
-//     console.log("Taile checken fry khaia kam nai");
-// } else
-// {
-//     console.log("Wow checken fry khabo moja kore");
-// }
-// if (chickenFryPrice < myBudget)
-// {
-//     console.log("Wow checken fry khabo moja kore");
-// } else
-// {
-//     console.log("Taile checken fry khaia kam nai");
-// }
+if (chickenFryPrice > myBudget)
+{
+    console.log("Taile checken fry khaia kam nai");
+} else
+{
+    console.log("Wow checken fry khabo moja kore");
+}
+if (chickenFryPrice < myBudget)
+{
+    console.log("Wow checken fry khabo moja kore");
+} else
+{
+    console.log("Taile checken fry khaia kam nai");
+}
 
-// if (chickenFryPrice < myBudget && potatoFry < myBudget)
-// {
-//     console.log("Yes Ajke chicken fry ar alur fry eksathe khabo");
-// } else
-// {
-//     console.log("Nah ajka kisui khamu na!");
-// }
+if (chickenFryPrice < myBudget && potatoFry < myBudget)
+{
+    console.log("Yes Ajke chicken fry ar alur fry eksathe khabo");
+} else
+{
+    console.log("Nah ajka kisui khamu na!");
+}
 
-// if (chickenFryPrice < myBudget || potatoFry < myBudget)
-// {
-//     console.log("Yes Jeta dame pushay seta khawa jabe");
-// } else
-// {
-//     console.log("Nah ajka kisui khamu na!");
-// }
+if (chickenFryPrice < myBudget || potatoFry < myBudget)
+{
+    console.log("Yes Jeta dame pushay seta khawa jabe");
+} else
+{
+    console.log("Nah ajka kisui khamu na!");
+}
 
-// var chelerChakri = false;
-// var chelerBari = true;
-// var chelerChoritroValo = false;
+var chelerChakri = false;
+var chelerBari = true;
+var chelerChoritroValo = false;
 
-// if (chelerChakri == true && chelerBari == true || chelerChoritroValo == true)
-// {
-//     console.log("Cheler Sathe amar meyer biye dibo, Cheler choritro kono matter kore na");
-// } else
-// {
-//     console.log("Cheler bari gari important, ta na thakle cheler loge meye re biya dimu na");
-// }
+if (chelerChakri == true && chelerBari == true || chelerChoritroValo == true)
+{
+    console.log("Cheler Sathe amar meyer biye dibo, Cheler choritro kono matter kore na");
+} else
+{
+    console.log("Cheler bari gari important, ta na thakle cheler loge meye re biya dimu na");
+}
 
 
 // Practice from habluder jonno programming
