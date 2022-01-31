f2735168cbec9f469443ad5afeb63ca9758f49b9
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2022-01-31 13:51:08 +0600

    complete practice of while loop

--- while-loop.js
+++ while-loop.js
@@ -1,31 +1,31 @@
-// var roastGiven = 0;
+var roastGiven = 0;
 
-// while (roastGiven < 7)
-// {
-//     console.log("Roast den please!!!")
-//     roastGiven++;
-//     console.log(roastGiven);
-// }
+while (roastGiven < 7)
+{
+    console.log("Roast den please!!!")
+    roastGiven++;
+    console.log(roastGiven);
+}
 
 // From Habluder Jonno Programming
-// var kotobar = 0;
 
-// while (kotobar < 10)
-// {
-//     console.log("kan dhore uthbos kortesi");
-//     kotobar++;
-//     console.log(kotobar);
-// }
+var kotobar = 0;
+while (kotobar < 10)
+{
+    console.log("kan dhore uthbos kortesi");
+    kotobar++;
+    console.log(kotobar);
+}
 
 // Task 7.1-Suppose your teacher gives you another punishment. Write a hundred times - "I will not poke anyone with a pen". Now write this program to write a hundred times with a while loop.
 
-// var write = 0;
-// while (write < 100)
-// {
-//     console.log("I will not poke anyone with a pen");
-//     write++;
-//     console.log(write);
-// }
+var write = 0;
+while (write < 100)
+{
+    console.log("I will not poke anyone with a pen");
+    write++;
+    console.log(write);
+}
 
 
 // Task 7.2-You have to send the message of Eid Mubarak to your fourteen friends by SMS. Write down that program.
