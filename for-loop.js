a60b9a2cf4deacf2ee861300feb8139661e49447
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2022-01-31 15:02:59 +0600

    complete for loop

---
+++ for-loop.js
@@ -0,0 +1,49 @@
+for (var i = 1; i <= 10; i++)
+{
+    console.log(i, "jon ke apple dewa hoise");
+}
+for (var i = 0; i < 10; i++)
+{
+    console.log(i);
+}
+
+
+for (var i = 1; i < 10; i++)
+{
+    console.log(i);
+}
+
+for (var i = 1; i <= 10; i++)
+{
+    console.log(i);
+}
+
+
+var joogfol = 0;
+for (var i = 1; i <= 100; i++)
+{
+    joogfol = joogfol + i;
+}
+
+console.log(joogfol);
+
+// Practice From Habluder Jonno Programming
+for (var i = 1; i <= 20; i++)
+{
+    console.log(i);
+}
+
+
+
+for (var i = 36; i <= 63; i++)
+{
+    console.log(i);
+}
+
+
+
+var joogfol = 0;
+for (var i = 1; i <= 100; i++)
+{
+    joogfol = joogfol + i;
+}
